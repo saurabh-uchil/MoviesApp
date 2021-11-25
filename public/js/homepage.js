554c9636da132a6c2ce2e7f387d99f98c68d019b
saurabh-uchil <uchilsaurabh@gmail.com>
2021-11-25 13:27:25 +1100

    View details done

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,11 +5,10 @@ $.getJSON(API_BASE_URL+'/discover/movie',api_options)
         /* console.log(movie) */
         const html = `
         <div style="float:left;color:whitesmoke;margin:10px; border: 2px solid black; width:350px; background: linear-gradient(147deg, #000000 0%, #04619f 95%);">
-        <img src="${IMAGE_URL + movie.poster_path}" style="margin:4px;">
-        <h4 style="margin:4px;">${movie.title}</h4>
-        <p style="margin:4px;">Release Date: ${movie.release_date}</p>
+        <img src="${IMAGE_URL + movie.poster_path}" style="margin:15px; height:450px; width:320px;">
+        <h4 style="margin:15px; font-size:16px;">${movie.title}</h4>
         <form method="get" action="/movies/${movie.id}">
-        <button style="height:40px; width:200px; background-color: darkblue; background-image: linear-gradient(darkblue,blue); border:2px solid yellow; color:whitesmoke;margin:4px; border-radius: 5px;">View Details</button>
+        <button style="height:40px; width:200px; background-color: darkblue; background-image: linear-gradient(darkblue,blue); border:2px solid yellow; color:whitesmoke;margin:15px; border-radius: 5px;">View Details</button>
         </form>
         
         </div>
--- public/js/singlemovie.js
+++ public/js/singlemovie.js
@@ -2,11 +2,13 @@ $.getJSON(API_BASE_URL+`/movie/${movie_id}`,api_options)
 .then((movie)=>{
     console.log(movie)
     movie.genres.forEach((genre)=>{
-        /* console.log(genre.name) */
         const moviegenre = `<p>${genre.name}</p>`
-        $('#genre').append(moviegenre)
+        $('#genres').append(moviegenre)
     })
     $('#title').text(movie.title)
+    $('#tagline').text(movie.tagline)
+    $('#runtime').text(movie.runtime)
+    $('#release_date').text(movie.release_date)
     $('#overview').append(movie.overview)
     $('#movieimg').attr({
         src: IMAGE_URL + movie.poster_path
