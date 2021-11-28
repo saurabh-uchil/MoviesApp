041ef16a9686c61e13904878e0d1a76f76ce09d6
Vikram Duvasi <vikramduvasi30@gmail.com>
2021-11-28 22:16:22 +1100

    commented the code for carosel and fixed few things to made code work. still need to fix styling

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/:id',(req,res)=>{
-    res.render('pages/movies',{
-        title:'Movies Page',
+router.get('/:id', (req, res) => {
+    res.render('pages/movies', {
+        title: 'Movies Page',
         movie_id: req.params.id
     })
 
-
-
+})
 
 module.exports = router
